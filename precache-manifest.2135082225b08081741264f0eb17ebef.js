98bc43dfdc49b2af2c3decbca90d35c8c2970234
Nazmul <grey000906@gmail.com>
2020-09-24 15:45:01 +0600

    Updates

---
+++ precache-manifest.2135082225b08081741264f0eb17ebef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c918fbf8ed914d72b31d32565506a98d",
+    "url": "/react-ToDo/index.html"
+  },
+  {
+    "revision": "f02044359b70487d05de",
+    "url": "/react-ToDo/static/css/main.4d37ba92.chunk.css"
+  },
+  {
+    "revision": "e627a8867f9878d73da6",
+    "url": "/react-ToDo/static/js/2.05f4b4b2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-ToDo/static/js/2.05f4b4b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f02044359b70487d05de",
+    "url": "/react-ToDo/static/js/main.5d5aa2f6.chunk.js"
+  },
+  {
+    "revision": "8c77df067ad391abb2e1",
+    "url": "/react-ToDo/static/js/runtime-main.f3a02e33.js"
+  }
+]);
